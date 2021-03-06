2bd55dc60da9019f3182cd34a451156cbcf07668
GunChleoc <fios@foramnagaidhlig.net>
2021-03-06 08:50:36 +0000

    Add af, gd and si locales

--- config/application.rb
+++ config/application.rb
@@ -50,6 +50,7 @@ module Mastodon
     # All translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [
+      :af,
       :ar,
       :ast,
       :bg,
@@ -72,6 +73,7 @@ module Mastodon
       :fi,
       :fr,
       :ga,
+      :gd,
       :gl,
       :he,
       :hi,
@@ -106,6 +108,7 @@ module Mastodon
       :ru,
       :sa,
       :sc,
+      :si,
       :sk,
       :sl,
       :sq,
